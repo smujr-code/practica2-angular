c1576770472dce400a188e09ef6df05c26c19f5a
Samuel Muj <smujr@miumg.edu.gt>
2026-09-20 12:40:11 -0600

    agregar estructura contador, practica 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
+  standalone: true,
   templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('practica2-angular');
+  contador: number = 0;
+
+  incrementar() {
+    this.contador++;
+  }
+
+  disminuir() {
+    this.contador--;
+  }
 }
